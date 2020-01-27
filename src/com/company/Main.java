5aeac4521a8f69e681b4c0f787479aed5d284ea5
bmargelos <57237131+bmargelos@users.noreply.github.com>
2020-01-27 02:35:01 -0500

    Update Main.java
    Added "Olá Mundo"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello, World");
-
         System.out.println("Hola! Mundo");
+        System.out.println("Olá Mundo");
+
 
 
     }
